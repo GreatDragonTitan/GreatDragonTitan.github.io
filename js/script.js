eabbd70c2519434caf2a988c41da59b4fffce4db
Titan <titanivan25@gmail.com>
2017-09-18 22:22:25 +0300

    Sixth version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+    function onMoveImg(a) {
+            a.width = '60%';
+            a.height = '60%';
+        }
+    function onOutImg(a) {
+            a.width = '50%';
+            a.height = '50%';
+        }
