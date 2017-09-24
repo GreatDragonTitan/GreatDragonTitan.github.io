bd0a2db4c427662309d86be57bcb9c0b77abe109
Titan <titanivan25@gmail.com>
2017-09-25 01:28:44 +0300

    Sevrnth version - not edited

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@
             a.width = '50%';
             a.height = '50%';
         }
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("c1").classList.toggle("change");
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("c1").classList.toggle("change");
+}
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
+function openNav1() {
+    
+    document.getElementById("myNav1").style.height = "100%";
+    
+}
+
+function closeNav1() {
+    document.getElementById("myNav1").style.height = "0%";
+}
+
